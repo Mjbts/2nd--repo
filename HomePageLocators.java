b40870b446caeb805f12510dbe4be9e652967889
SathishMerugu <sathishmerugu70@gamil.com>
2026-01-13 05:59:49 +0530

    Selenium Project

---
+++ HomePageLocators.java
@@ -0,0 +1,18 @@
+package com.locators.HRM;
+
+
+
+public class HomePageLocators 
+{
+
+	public static final String loginBrandingXpath = "//div[@class='orangehrm-login-branding']";
+	public static final String LoginHeaderXpath = "//h5[normalize-space()='Login']";
+	public static final String AdminXpath ="//p[normalize-space()='Username : Admin']";
+	public static final String adminpass = "//p[normalize-space()='Password : admin123']";
+	public static final String UserName = "//input[@placeholder='Username']";
+	public static final String Password ="//input[@placeholder='Password']";
+	public static final String loginBtn = "//button[normalize-space()='Login']";
+	public static final String forgotPass = "//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']";
+	public static final String Logo = "//div[@class='orangehrm-login-logo']//img[@alt='orangehrm-logo']";
+
+}
